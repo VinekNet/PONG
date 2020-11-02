721ada55a2d0a08dcb3d0ef5703fa850bafda8a1
VinekNet <I.am.Minded.7@gmail.com>
2020-11-02 16:02:11 +0100

    Cours 1

---
+++ js/pong.js
@@ -0,0 +1,12 @@
+let largeur=$("#balle").width();
+let gauche=parseInt($("#balle").css("left"));
+let haut=parseInt($("#balle").css("top"));
+alert(gauche)
+
+
+setInterval(function(){
+    gauche++;
+    haut=haut+0.5;
+    $("#balle").css("left",gauche);
+    $("#balle").css("top",haut);
+}, 10);
