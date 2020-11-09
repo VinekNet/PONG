66e194096cc4d96cf7d8bae1077178995912c3a2
VinekNet <I.am.Minded.7@gmail.com>
2020-11-09 16:20:19 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -2,7 +2,7 @@
 //classe du Terrain
 class Terrain{
     constructor($element){
-        this.$element=$element
+        this.$element=$element;
         this.largeur=$element.width();
         this.hauteur=$element.height();    
     }
